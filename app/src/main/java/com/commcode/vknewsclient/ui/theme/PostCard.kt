71b7584c6a65fea9501ecb794b0c061abe7cd336
DennsHartstock <dennis.hartstock@web.de>
2023-04-03 16:31:00 +0200

    finish post card layout

--- app/src/main/java/com/commcode/vknewsclient/ui/theme/PostCard.kt
+++ app/src/main/java/com/commcode/vknewsclient/ui/theme/PostCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,31 +22,89 @@ import com.commcode.vknewsclient.R
 @Composable
 fun PostCard() {
     Card {
+        Column(modifier = Modifier.padding(8.dp)) {
+            PostHeader()
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Post content")
+            Spacer(modifier = Modifier.height(8.dp))
+            Image(
+                painter = painterResource(id = R.drawable.post_content_image),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Statistics()
+        }
+    }
+}
+
+@Composable
+private fun Statistics() {
+    Row {
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
+            modifier = Modifier.weight(1f),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                modifier = Modifier
-                    .size(50.dp)
-                    .clip(CircleShape),
-                painter = painterResource(id = R.drawable.post_community_thumbnail),
-                contentDescription = "/dev/null"
+            IconWithText(iconResId = R.drawable.ic_views_count, text = "8")
+        }
+        Row(
+            modifier = Modifier.weight(1f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconWithText(iconResId = R.drawable.ic_share, text = "23")
+            IconWithText(iconResId = R.drawable.ic_comment, text = "10")
+            IconWithText(iconResId = R.drawable.ic_like, text = "15")
+        }
+    }
+}
+
+@Composable
+private fun IconWithText(iconResId: Int, text: String) {
+    Row {
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colors.onSecondary
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = text, color = MaterialTheme.colors.onSecondary)
+    }
+}
+
+@Composable
+private fun PostHeader() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape),
+            painter = painterResource(
+                id = R.drawable.post_community_thumbnail
+            ),
+            contentDescription = "/dev/null"
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = "Community Title",
+                color = MaterialTheme.colors.onPrimary
             )
-            Spacer(modifier = Modifier.padding(8.dp))
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "Community Title", color = MaterialTheme.colors.onPrimary)
-                Spacer(modifier = Modifier.padding(4.dp))
-                Text(text = "09:24", color = MaterialTheme.colors.onSecondary)
-            }
-            Icon(
-                imageVector = Icons.Rounded.MoreVert,
-                contentDescription = "Menu",
-                tint = MaterialTheme.colors.onSecondary
+            Spacer(modifier = Modifier.padding(4.dp))
+            Text(
+                text = "09:24",
+                color = MaterialTheme.colors.onSecondary
             )
         }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = "Menu",
+            tint = MaterialTheme.colors.onSecondary
+        )
     }
 }
 
