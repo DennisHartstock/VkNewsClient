faa84031ad3324b5a483a247f71bfff3abdb1350
DennsHartstock <dennis.hartstock@web.de>
2023-04-05 09:38:18 +0200

    using state

--- app/src/main/java/com/commcode/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/commcode/vknewsclient/MainActivity.kt
@@ -3,14 +3,7 @@ package com.commcode.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.commcode.vknewsclient.ui.theme.PostCard
+import com.commcode.vknewsclient.ui.theme.MainScreen
 import com.commcode.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background)
-                        .padding(8.dp)
-                ) {
-                    PostCard()
-                }
+                MainScreen()
             }
         }
     }
