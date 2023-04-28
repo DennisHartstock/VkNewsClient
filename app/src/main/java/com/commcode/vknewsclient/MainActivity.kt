0f63001c3eb86b3a8ccc868034263364be9e4828
DennsHartstock <dennis.hartstock@web.de>
2023-04-28 08:20:16 +0200

    Add VK auth

--- app/src/main/java/com/commcode/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/commcode/vknewsclient/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.commcode.vknewsclient
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import com.commcode.vknewsclient.ui.theme.MainScreen
 import com.commcode.vknewsclient.ui.theme.VkNewsClientTheme
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : ComponentActivity() {
 
@@ -12,6 +17,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
+                val launcher = rememberLauncherForActivityResult(
+                    contract = VK.getVKAuthActivityResultContract()
+                ) {
+                    when (it) {
+                        is VKAuthenticationResult.Success -> {
+                            Log.d("MainActivity", "Success auth")
+                        }
+
+                        is VKAuthenticationResult.Failed -> {
+                            Log.d("MainActivity", "Failed auth")
+                        }
+                    }
+                }
+                launcher.launch(listOf(VKScope.WALL))
                 MainScreen()
             }
         }
